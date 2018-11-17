a3236db7ffbd0b929314e9ea59f5ffef08fdce09
Therdpong <therdpong1996@icloud.com>
2018-11-17 16:26:19 +0700

    remove credit

--- views/parts/footer.content.php
+++ views/parts/footer.content.php
@@ -4,7 +4,7 @@
       <div class="row align-items-center justify-content-xl-between">
         <div class="col-xl-6">
           <div class="copyright text-center text-xl-left text-muted">
-            &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+            &copy; 2018
           </div>
         </div>
         <div class="col-xl-6">
