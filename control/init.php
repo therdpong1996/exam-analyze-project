a0115b7ca28e721e99e1eb364bd88c59452248ed
Jay <pakpumwittaya@gmail.com>
2018-11-24 14:20:22 +0700

    Fix Javascript

--- control/init.php
+++ control/init.php
@@ -25,6 +25,6 @@
     }
 
     //WEB URL  
-    $_G['url'] = 'https://exam-analyze.herokuapp.com/';
+    $_G['url'] = 'http://localhost/project/';
 
     require_once 'functions.php';
--- views/parts/footer.common.php
+++ views/parts/footer.common.php
@@ -1,12 +1,7 @@
 
-  <script src="<?php echo $_G['url'];?>assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="<?php echo $_G['url'];?>assets/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-  <!-- Optional JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+
   <script src="<?php echo $_G['url'];?>assets/js/argon.js?v=1.0.0"></script>
-  <script src="<?php echo $_G['url'];?>assets/js/sweetalert2.all.min.js?v=1.0.0"></script>
-  <script src="<?php echo $_G['url'];?>assets/dist/easytimer.min.js"></script>
-  <script src="<?php echo $_G['url'];?>assets/dist/js.cookie-2.2.0.min.js"></script>
+
 
   <?php if($user_row['role'] == 1){ ?>
   	<script src="<?php echo $_G['url'];?>js/role_1.apps.js?v=1.0.0"></script>
