c4c9192e553de1664a9ed9a75f15f085b0894009
Therdpong Khangrang <numlifez@icloud.com>
2018-12-03 19:19:23 +0700

    Update footer.content.php

--- views/parts/footer.content.php
+++ views/parts/footer.content.php
@@ -4,7 +4,7 @@
       <div class="row align-items-center justify-content-xl-between">
         <div class="col-xl-6">
           <div class="copyright text-center text-xl-left text-muted">
-            &copy; 2018 <a href="#" class="font-weight-bold ml-1" target="_blank">EXAM-ANALYZE <?php echo trim(exec('git log --pretty="%h" -n1 HEAD')); ?></a>
+            &copy; 2018 <a href="#" class="font-weight-bold ml-1" target="_blank">EXAM-ANALYZE</a>
           </div>
         </div>
         <div class="col-xl-6">
