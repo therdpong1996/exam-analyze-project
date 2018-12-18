79db155a636494d949d57d397a90305f15a779fe
Therdpong Khangrang <therdpong1996@icloud.com>
2018-12-18 11:33:25 +0700

    Update setting.pageadmin.php

--- views/setting.pageadmin.php
+++ views/setting.pageadmin.php
@@ -7,7 +7,7 @@
         <a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="<?php url('admin/setting'); ?>">Website Setting <small>(ตั้งค่า)</small></a>
         <?php require_once 'parts/usermenu.common.php'; ?>
         
-        <div class="container-fluid pb-8 pt-5 pt-md-8">
+        <div class="container-fluid pb-5 pt-5 pt-md-8">
             <div class="row">
                 <div class="col-xl-12">
                     <div class="card shadow">
@@ -19,9 +19,9 @@
                                     <label class="col-sm-2 col-form-label" for="init_regis">ระบบสมัครสมาชิก</label>
                                     <div class="col-sm-10">
                                         <label class="custom-toggle">
-                                            <input type="checkbox" id="init_regis" name="init_regis" value="1" <?php __($_GS['init_regis']==1?'checked':''); ?>>
+                                            <input type="checkbox" class="setting_sw" id="init_regis" name="init_regis" value="1" <?php __($_GS['init_regis']==1?'checked':''); ?>>
                                             <span class="custom-toggle-slider rounded-circle"></span>
-                                        </label>
+                                        </label><br>
                                         <small class="text-muted">เปิด-ปิด ระบบสมัครสมาชิกเว็บไซต์</small>
                                     </div>
                                 </div>
@@ -29,9 +29,9 @@
                                     <label class="col-sm-2 col-form-label" for="init_article">ระบบบทความ</label>
                                     <div class="col-sm-10">
                                         <label class="custom-toggle">
-                                            <input type="checkbox" id="init_article" name="init_article" value="1" <?php __($_GS['init_article']==1?'checked':''); ?>>
+                                            <input type="checkbox" class="setting_sw" id="init_article" name="init_article" value="1" <?php __($_GS['init_article']==1?'checked':''); ?>>
                                             <span class="custom-toggle-slider rounded-circle"></span>
-                                        </label>
+                                        </label><br>
                                         <small class="text-muted">เปิด-ปิด การเพิ่ม แก้ไข และลบบทความ</small>
                                     </div>
                                 </div>
@@ -39,9 +39,9 @@
                                     <label class="col-sm-2 col-form-label" for="init_subject">ระบบรายวิชา</label>
                                     <div class="col-sm-10">
                                         <label class="custom-toggle">
-                                            <input type="checkbox" id="init_subject" name="init_subject" value="1" <?php __($_GS['init_subject']==1?'checked':''); ?>>
+                                            <input type="checkbox" class="setting_sw" id="init_subject" name="init_subject" value="1" <?php __($_GS['init_subject']==1?'checked':''); ?>>
                                             <span class="custom-toggle-slider rounded-circle"></span>
-                                        </label>
+                                        </label><br>
                                         <small class="text-muted">เปิด-ปิด การเพิ่ม แก้ไข และลบรายวิชา</small>
                                     </div>
                                 </div>
@@ -49,9 +49,9 @@
                                     <label class="col-sm-2 col-form-label" for="init_exam">ระบบคลังข้อสอบ</label>
                                     <div class="col-sm-10">
                                         <label class="custom-toggle">
-                                            <input type="checkbox" id="init_exam" name="init_exam" value="1" <?php __($_GS['init_exam']==1?'checked':''); ?>>
+                                            <input type="checkbox" class="setting_sw" id="init_exam" name="init_exam" value="1" <?php __($_GS['init_exam']==1?'checked':''); ?>>
                                             <span class="custom-toggle-slider rounded-circle"></span>
-                                        </label>
+                                        </label><br>
                                         <small class="text-muted">เปิด-ปิด การเพิ่ม แก้ไข และลบคลังข้อสอบ</small>
                                     </div>
                                 </div>
@@ -59,9 +59,9 @@
                                     <label class="col-sm-2 col-form-label" for="init_session">ระบบเซสชั่น</label>
                                     <div class="col-sm-10">
                                         <label class="custom-toggle">
-                                            <input type="checkbox" id="init_session" name="init_session" value="1" <?php __($_GS['init_session']==1?'checked':''); ?>>
+                                            <input type="checkbox" class="setting_sw" id="init_session" name="init_session" value="1" <?php __($_GS['init_session']==1?'checked':''); ?>>
                                             <span class="custom-toggle-slider rounded-circle"></span>
-                                        </label>
+                                        </label><br>
                                         <small class="text-muted">เปิด-ปิด การเพิ่ม แก้ไข และลบเซสชั่น</small>
                                     </div>
                                 </div>
@@ -69,21 +69,21 @@
                                     <label class="col-sm-2 col-form-label" for="init_testing">ระบบการทดสอบ</label>
                                     <div class="col-sm-10">
                                         <label class="custom-toggle">
-                                            <input type="checkbox" id="init_testing" name="init_testing" value="1" <?php __($_GS['init_testing']==1?'checked':''); ?>>
+                                            <input type="checkbox" class="setting_sw" id="init_testing" name="init_testing" value="1" <?php __($_GS['init_testing']==1?'checked':''); ?>>
                                             <span class="custom-toggle-slider rounded-circle"></span>
-                                        </label>
+                                        </label><br>
                                         <small class="text-muted">เปิด-ปิด ระบบการทดสอบของนักศึกษา</small>
                                     </div>
                                 </div>
                                 <h2>ตั้งค่า Adaptive</h2>
                                 <hr>
                                 <div class="form-group row">
-                                    <label class="col-sm-2 col-form-label" for="init_graph">ระบบ train data and  graph</label>
-                                    <div class="col-sm-10">
+                                    <label class="col-sm-3 col-form-label" for="init_graph">ระบบ train data and  graph</label>
+                                    <div class="col-sm-9">
                                         <label class="custom-toggle">
-                                            <input type="checkbox" id="init_graph" name="init_graph" value="1" <?php __($_GS['init_graph']==1?'checked':''); ?>>
+                                            <input type="checkbox" class="setting_sw" id="init_graph" name="init_graph" value="1" <?php __($_GS['init_graph']==1?'checked':''); ?>>
                                             <span class="custom-toggle-slider rounded-circle"></span>
-                                        </label>
+                                        </label><br>
                                         <small class="text-muted">เปิด-ปิด ระบบการทดสอบของนักศึกษา</small>
                                     </div>
                                 </div>
@@ -93,3 +93,8 @@
                 </div>
             </div>
         </div>
+        <script>
+            $('#setting_sw').on('change', function () {
+                console.log($('#setting-form').serialize())
+            })
+        </script>
