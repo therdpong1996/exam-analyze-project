21c12a72beb19c185f72332afef9ca8f088116b9
Therdpong Khangrang <therdpong1996@icloud.com>
2018-12-19 12:07:33 +0700

    Update temail-register.php

--- ajax/temail-register.php
+++ ajax/temail-register.php
@@ -20,14 +20,4 @@
 
         $mgClient = new Mailgun('c6a6fb3027866dd672043e123c011a2e-9b463597-b03543b2');
         $domain = "sandbox9cf778143713409097470f85c022fee9.mailgun.org";
-        $text = 'Hello, Verify URL: '.$vurl;
-        $html = '<strong>Hello,</strong><br><p>Verify URL : <a href="'.$vurl.'" target="_blank">'.$vurl.'</a></p><br><br><small>CAT@RMUTL<br>'.$_G['url'].'</small>';
-        $result = $mgClient->sendMessage($domain,
-            [   'from' => 'Mailgun Sandbox <postmaster@sandbox9cf778143713409097470f85c022fee9.mailgun.org>',
-                'to' => 'Anonymouse <'.$email.'>',
-                'subject' => 'Hello '.$email,
-                'text' => $text,
-                'html' => $html
-            ]
-        );
     }
