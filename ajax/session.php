7f78f90c1e38b3e65be3bd6e64bd60b2c70beca9
Therdpong Khangrang <therdpong1996@icloud.com>
2018-12-23 12:01:16 +0700

    Update session.php

--- ajax/session.php
+++ ajax/session.php
@@ -43,10 +43,17 @@
         $timeleft = $_POST['session_timeleft'];
         $password = $_POST['session_password'];
         $solve = (isset($_POST['session_solve']) ? '1' : '0');
+        $number = (isset($_POST['session_adap_number']) ? $_POST['session_adap_number'] : '0');
         $import = $_POST['adaptimport'];
         $adap_active = $_POST['session_adap'];
 
-        $stm = $_DB->prepare('INSERT INTO sessions (session_exam,session_password,session_timeleft,session_start,session_end,session_solve,session_adap,session_adap_active) VALUES (:exam, :password, :timeleft, :start, :end, :solve, :adap, :active)');
+        if($import != 0){
+            $train = 1;
+        }else{
+            $train = 0;
+        }
+
+        $stm = $_DB->prepare('INSERT INTO sessions (session_exam,session_password,session_timeleft,session_start,session_end,session_solve,session_adap,session_adap_active,session_train,session_adap_number) VALUES (:exam, :password, :timeleft, :start, :end, :solve, :adap, :active, :train, :number)');
         $stm->bindParam(':exam', $exam, PDO::PARAM_INT);
         $stm->bindParam(':start', $start, PDO::PARAM_STR);
         $stm->bindParam(':end', $end, PDO::PARAM_STR);
@@ -55,6 +62,8 @@
         $stm->bindParam(':solve', $solve, PDO::PARAM_INT);
         $stm->bindParam(':adap', $import, PDO::PARAM_INT);
         $stm->bindParam(':active', $adap_active, PDO::PARAM_INT);
+        $stm->bindParam(':train', $train, PDO::PARAM_INT);
+        $stm->bindParam(':number', $number, PDO::PARAM_INT);
         $stm->execute();
         $lastid = $_DB->lastInsertId();
 
@@ -97,8 +106,13 @@
         $number = $_POST['session_adap_number'];
         $import = $_POST['adaptimport'];
         $adap_active = (isset($_POST['session_adap']) ? '1' : '0');
+        if($import != 0){
+            $train = 1;
+        }else{
+            $train = 0;
+        }
 
-        $stm = $_DB->prepare('UPDATE sessions SET session_exam = :exam, session_password = :password, session_timeleft = :timeleft, session_start = :start, session_end = :end, session_solve = :solve, session_adap = :adap, session_adap_active = :active, session_adap_number = :number WHERE session_id = :session_id');
+        $stm = $_DB->prepare('UPDATE sessions SET session_exam = :exam, session_password = :password, session_timeleft = :timeleft, session_start = :start, session_end = :end, session_solve = :solve, session_adap = :adap, session_adap_active = :active, session_adap_number = :number, session_train = :train WHERE session_id = :session_id');
         $stm->bindParam(':exam', $exam, PDO::PARAM_INT);
         $stm->bindParam(':start', $start, PDO::PARAM_STR);
         $stm->bindParam(':end', $end, PDO::PARAM_STR);
@@ -109,6 +123,7 @@
         $stm->bindParam(':active', $adap_active, PDO::PARAM_INT);
         $stm->bindParam(':number', $number, PDO::PARAM_INT);
         $stm->bindParam(':session_id', $id, PDO::PARAM_INT);
+        $stm->bindParam(':train', $train, PDO::PARAM_INT);
         $stm->execute();
 
         echo json_encode(['state' => true, 'msg' => 'แก้ไข Session แล้ว']);
