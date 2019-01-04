56bcbf0783f238caf78c32bd6e84234df2e059a3
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-04 12:46:23 +0700

    Update search_adaptive.php

--- ajax/search_adaptive.php
+++ ajax/search_adaptive.php
@@ -22,10 +22,8 @@ $stm = $_DB->prepare("SELECT adap_id,std_number,time_update FROM adaptive_table 
 $stm->bindParam(":examid", $_POST['examid']);
 $stm->execute();
 $row = $stm->fetch(PDO::FETCH_ASSOC);
-$nrow = $stm->fetchColumn();
 
-
-if ($nrow >= 1) {
+if ($row['adap_id']) {
     echo json_encode(['state' => true, 'msg' => 'พอข้อมูลการวิเคราะห์ของข้อสอบชุดนี้ ผู้ทดสอบจำนวน '.$row['std_number'].' คน อัพเดทเมื่อ '.$row['time_update'].'<br>ต้องการ Import หรือไม่ ?<br><button class="btn btn-success" onclick="addAdaptid('.$row['adap_id'].')">Import</button> <button class="btn btn-danger" onclick="addAdaptid(0)">ไม่ Import</button>']);
     exit;
 }else{
