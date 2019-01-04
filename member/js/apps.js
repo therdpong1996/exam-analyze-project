6e99c2fea58490240b029f838322b0567dcd4c00
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-04 16:19:30 +0700

    Update apps.js

--- member/js/apps.js
+++ member/js/apps.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
     $('.dataTable').DataTable();
 });
+
+setInterval(() => {
+    doesConnectionExist();
+}, 10000);
+
+function doesConnectionExist() {
+    var xhr = new XMLHttpRequest();
+    var file = "https://www.kirupa.com/blank.png";
+    var randomNum = Math.round(Math.random() * 10000);
+
+    xhr.open('HEAD', file + "?rand=" + randomNum, true);
+    xhr.send();
+
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest(e) {
+        if (xhr.readyState == 4) {
+            if (xhr.status >= 200 && xhr.status < 304) {
+                alert("connection exists!");
+            } else {
+                alert("connection doesn't exist!");
+            }
+        }
+    }
+}
