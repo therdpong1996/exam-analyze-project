cd52ccd9592179bf241af584785874b094ac222e
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-05 15:18:33 +0700

    Update dashboard.page.php

--- member/views/dashboard.page.php
+++ member/views/dashboard.page.php
@@ -33,18 +33,18 @@
                                 $stmt->execute();
                                 $row = $stmt->fetch(PDO::FETCH_ASSOC);    
                             ?>
-                            <div class="mt-3">
+                            <div class="mb-3">
                                 <div class="row">
-                                    <div class="col-1">
+                                    <div class="col-1 text-center pt-1">
                                         <div class="avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>"></div>
                                     </div>
-                                    <div class="col-11">
+                                    <div class="col-11 pl-1">
                                         <?php __($rows['full_name']); ?><br><small class="text-muted"><?php __($rows['ontime']); ?></small>
                                     </div>
                                 </div>    
                             </div>
                             <p>ได้สร้างบทความใหม่ "<?php __($row['title']); ?>"</p>
-                            <p>รายวิชา "<?php __($row['subject_title']); ?>"</p>
+                            <p>ในรายวิชา "<?php __($row['subject_title']); ?>"</p>
                             <a href="#" class="btn btn-info">อ่าน</a>
                             <?php }elseif($rows['type']=='exam'){
                                 $stmt = $_DB->prepare("SELECT examinations.examination_id,examinations.examination_title,subjects.subject_title FROM examinations JOIN subjects ON examinations.examination_subject = subjects.subject_id WHERE examinations.examination_id = :id");
@@ -52,20 +52,36 @@
                                 $stmt->execute();
                                 $row = $stmt->fetch(PDO::FETCH_ASSOC);    
                             ?>
-                            <small class="text-muted"><?php __($rows['ontime']); ?></small>
-                            <div class="mt-3"><div class="mr-2 avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>" /></div><?php __($rows['full_name']); ?></div>
+                            <div class="mb-3">
+                                <div class="row">
+                                    <div class="col-1 text-center pt-1">
+                                        <div class="avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>"></div>
+                                    </div>
+                                    <div class="col-11 pl-1">
+                                        <?php __($rows['full_name']); ?><br><small class="text-muted"><?php __($rows['ontime']); ?></small>
+                                    </div>
+                                </div>    
+                            </div>
                             <p>ได้สร้างชุดข้อสอบใหม่ "<?php __($row['examination_title']); ?>"</p>
-                            <p>รายวิชา "<?php __($row['subject_title']); ?>"</p>
+                            <p>ในรายวิชา "<?php __($row['subject_title']); ?>"</p>
                             <?php }elseif($rows['type']=='session'){
                                 $stmt = $_DB->prepare("SELECT sessions.session_id,sessions.session_adap_active,examinations.examination_title,subjects.subject_title FROM sessions JOIN examinations ON sessions.session_exam = examinations.examination_id JOIN subjects ON examinations.examination_subject = subjects.subject_id WHERE sessions.session_id = :id");
                                 $stmt->bindParam(':id', $rows['content_id']);
                                 $stmt->execute();
                                 $row = $stmt->fetch(PDO::FETCH_ASSOC);    
                             ?>
-                            <small class="text-muted"><?php __($rows['ontime']); ?></small>
-                            <div class="mt-3"><div class="mr-2 avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>" /></div><?php __($rows['full_name']); ?></div>
+                            <div class="mb-3">
+                                <div class="row">
+                                    <div class="col-1 text-center pt-1">
+                                        <div class="avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>"></div>
+                                    </div>
+                                    <div class="col-11 pl-1">
+                                        <?php __($rows['full_name']); ?><br><small class="text-muted"><?php __($rows['ontime']); ?></small>
+                                    </div>
+                                </div>    
+                            </div>
                             <p>ได้สร้างเซสชั่นการทำข้อสอบของชุดข้อสอบ "<?php __($row['examination_title']); ?>"</p>
-                            <p>รายวิชา "<?php __($row['subject_title']); ?>"</p>
+                            <p>ในรายวิชา "<?php __($row['subject_title']); ?>"</p>
 
                             <?php }elseif($rows['type']=='train'){
                                 $stmt = $_DB->prepare("SELECT adaptive_table.std_number,examinations.examination_title,subjects.subject_title FROM adaptive_table JOIN examinations ON adaptive_table.exam_id = examinations.examination_id JOIN subjects ON examinations.examination_subject = subjects.subject_id WHERE adaptive_table.score_id = :id");
@@ -73,10 +89,18 @@
                                 $stmt->execute();
                                 $row = $stmt->fetch(PDO::FETCH_ASSOC);    
                             ?>
-                            <small class="text-muted"><?php __($rows['ontime']); ?></small>
-                            <div class="mt-3"><div class="mr-2 avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>" /></div><?php __($rows['full_name']); ?></div>
+                            <div class="mb-3">
+                                <div class="row">
+                                    <div class="col-1 text-center pt-1">
+                                        <div class="avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>"></div>
+                                    </div>
+                                    <div class="col-11 pl-1">
+                                        <?php __($rows['full_name']); ?><br><small class="text-muted"><?php __($rows['ontime']); ?></small>
+                                    </div>
+                                </div>    
+                            </div>
                             <p>ได้สร้าง Train ข้อมูลของข้อสอบชุด "<?php __($row['examination_title']); ?>"</p>
-                            <p>รายวิชา "<?php __($row['subject_title']); ?>"</p>
+                            <p>ในรายวิชา "<?php __($row['subject_title']); ?>"</p>
 
                             <?php }elseif($rows['type']=='solve'){
                                 $stmt = $_DB->prepare("SELECT session_score.score,session_score.score_full,examinations.examination_title,subjects.subject_title FROM session_score JOIN examinations ON session_score.exam_id = examinations.examination_id JOIN subjects ON session_score.subject_id = subjects.subject_id WHERE session_score.score_id = :id");
@@ -84,10 +108,18 @@
                                 $stmt->execute();
                                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
                             ?>
-                            <small class="text-muted"><?php __($rows['ontime']); ?></small>
-                            <div class="mt-3"><div class="mr-2 avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>" /></div><?php __($rows['full_name']); ?></div>
+                            <div class="mb-3">
+                                <div class="row">
+                                    <div class="col-1 text-center pt-1">
+                                        <div class="avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>"></div>
+                                    </div>
+                                    <div class="col-11 pl-1">
+                                        <?php __($rows['full_name']); ?><br><small class="text-muted"><?php __($rows['ontime']); ?></small>
+                                    </div>
+                                </div>    
+                            </div>
                             <p>ได้ทำข้อสอบ "<?php __($row['examination_title']); ?>"</p>
-                            <p>รายวิชา "<?php __($row['subject_title']); ?>"</p>
+                            <p>ในรายวิชา "<?php __($row['subject_title']); ?>"</p>
                             <p>ได้คะแนน <?php __($row['score']); ?>/<?php __($row['score_full']); ?></p>
                             <?php }elseif($rows['type']=='solve-a'){
                                 $stmt = $_DB->prepare("SELECT adaptive_session_score.score,adaptive_session_score.score_full,examinations.examination_title,subjects.subject_title FROM adaptive_session_score JOIN examinations ON adaptive_session_score.exam_id = examinations.examination_id JOIN subjects ON adaptive_session_score.subject_id = subjects.subject_id WHERE adaptive_session_score.score_id = :id");
@@ -95,10 +127,18 @@
                                 $stmt->execute();
                                 $row = $stmt->fetch(PDO::FETCH_ASSOC);
                             ?>
-                            <small class="text-muted"><?php __($rows['ontime']); ?></small>
-                            <div class="mt-3"><div class="mr-2 avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>" /></div><?php __($rows['full_name']); ?></div>
-                            <p>ได้สร้าง Train ข้อมูลของข้อสอบชุด "<?php __($row['examination_title']); ?>"</p>
-                            <p>รายวิชา "<?php __($row['subject_title']); ?>"</p>
+                            <div class="mb-3">
+                                <div class="row">
+                                    <div class="col-1 text-center pt-1">
+                                        <div class="avatar avatar-sm rounded-circle"><img avatar="<?php echo $rows['email']; ?>"></div>
+                                    </div>
+                                    <div class="col-11 pl-1">
+                                        <?php __($rows['full_name']); ?><br><small class="text-muted"><?php __($rows['ontime']); ?></small>
+                                    </div>
+                                </div>    
+                            </div>
+                            <p>ได้ทำข้อสอบ "<?php __($row['examination_title']); ?>"</p>
+                            <p>ในรายวิชา "<?php __($row['subject_title']); ?>"</p>
                             <p>ได้คะแนน <?php __($row['score']); ?>/<?php __($row['score_full']); ?></p>
                             <?php } ?>
                         </div>
