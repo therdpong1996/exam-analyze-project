479686b4f875d0820f1bab38547202cd7a37cf84
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-18 15:37:05 +0700

    Update header.php

--- views/header.php
+++ views/header.php
@@ -21,7 +21,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $_G['furl']; ?>assets/icon//favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $_G['furl']; ?>assets/icon//favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $_G['furl']; ?>assets/icon//favicon-16x16.png">
-    <link rel="manifest" href="<?php echo $_G['furl']; ?>assets/icon//manifest.json">
+    <link rel="manifest" href="<?php echo $_G['furl']; ?>manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="<?php echo $_G['furl']; ?>assets/icon//ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
