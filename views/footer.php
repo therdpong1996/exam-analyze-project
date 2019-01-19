62ef3b9299ea4d522ff71b2bf03fd4d6d2161ea5
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-19 13:21:04 +0700

    update sw

--- views/footer.php
+++ views/footer.php
@@ -32,7 +32,7 @@
             console.log('[PWA] active service worker found, no need to register')
         } else {
         //Register the ServiceWorker
-        navigator.serviceWorker.register('pwabuilder-sw.js', {
+        navigator.serviceWorker.register('service-worker.js', {
             scope: './'
         }).then(function(reg) {
             console.log('Service worker has been registered for scope:'+ reg.scope);
