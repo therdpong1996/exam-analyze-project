c768867a8780915c56e0d3ad92018ac1a23632f8
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-19 17:51:50 +0700

    Update navbar.php

--- views/navbar.php
+++ views/navbar.php
@@ -1,9 +1,9 @@
 
     <header class="header-global">
-        <nav id="navbar-main" class="navbar navbar-main navbar-expand-lg navbar-cat navbar-light headroom">
+        <nav id="navbar-main" class="navbar navbar-main fixed-top navbar-expand-lg navbar-cat navbar-light headroom">
         <div class="container">
             <a class="navbar-brand mr-lg-5" href="<?php echo $_G['furl']; ?>">
-            <img src="<?php furl('assets/img/brand/white.png'); ?>">
+            <img src="<?php furl('assets/img/brand/blue.png'); ?>">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
