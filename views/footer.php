daa124e481113f13fec792cea81d6d8087b638d8
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-19 13:17:32 +0700

    fix pwa 1

--- views/footer.php
+++ views/footer.php
@@ -29,7 +29,7 @@
     <script>
         //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
         if (navigator.serviceWorker.controller) {
-        console.log('[PWA Builder] active service worker found, no need to register')
+            console.log('[PWA] active service worker found, no need to register')
         } else {
         //Register the ServiceWorker
         navigator.serviceWorker.register('pwabuilder-sw.js', {
--- views/header.php
+++ views/header.php
@@ -3,27 +3,26 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Start your development with a Design System for Bootstrap 4.">
-    <meta name="author" content="Creative Tim">
     <title><?php echo $meta['title']; ?></title>
 
     <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $_G['furl']; ?>assets/icon//apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $_G['furl']; ?>assets/icon//android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $_G['furl']; ?>assets/icon//favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $_G['furl']; ?>assets/icon//favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $_G['furl']; ?>assets/icon//favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $_G['furl']; ?>assets/icon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $_G['furl']; ?>assets/icon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $_G['furl']; ?>assets/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $_G['furl']; ?>assets/icon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $_G['furl']; ?>assets/icon/favicon-16x16.png">
     <link rel="manifest" href="<?php echo $_G['furl']; ?>manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?php echo $_G['furl']; ?>assets/icon//ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?php echo $_G['furl']; ?>assets/icon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
     <link href="<?php echo $_G['furl']; ?>assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
