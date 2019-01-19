e268ca79b56b19396c7731b4cc5d3d53f18c63aa
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-19 17:55:55 +0700

    Update navbar.php

--- views/navbar.php
+++ views/navbar.php
@@ -5,22 +5,8 @@
             <a class="navbar-brand mr-lg-5" href="<?php echo $_G['furl']; ?>">
             <img src="<?php furl('assets/img/brand/blue.png'); ?>">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
+            <a href="<?php url(null); ?>" class="btn d-lg-none btn-primary btn-sm">เข้าสู่ระบบ</a>
             <div class="navbar-collapse collapse" id="navbar_global">
-            <div class="navbar-collapse-header">
-                <div class="row">
-                <div class="col-6 collapse-brand">
-                    <a href="<?php echo $_G['furl']; ?>">
-                    <img src="<?php furl('assets/img/brand/blue.png'); ?>">
-                    </a>
-                </div>
-                <div class="col-6 collapse-close">
-                    <a href="<?php url(null); ?>" class="btn d-lg-none btn-primary btn-sm">เข้าสู่ระบบ</a>
-                </div>
-                </div>
-            </div>
             <ul class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item d-lg-block ml-lg-4">
                 <a href="<?php url(null); ?>" class="btn btn-neutral btn-icon">
