96883df94f52cb540eb319e9e578346027d82d76
Therdpong Khangrang <therdpong1996@icloud.com>
2019-01-22 15:24:28 +0700

    Update apps.js

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -16,7 +16,7 @@ function readArticle(atid) {
     window.location.hash = '#' + atid;
     $('#content-rows').fadeOut(200);
     setTimeout(() => {
-        db.collection("articles").doc(atid).get().then((querySnapshot) => {
+        db.collection("articles").doc(atid.toString()).get().then((querySnapshot) => {
             $('#content-rows').html('<button class="btn btn-info" onclick="initialApp()"><i class="fa fa-arrow-left"></i> กลับ</button><div class="card shadow mt-3"><div class="card-header"><h2 class="mb-0">' + querySnapshot.data().title + '</h2></div><div class="card-body">' + querySnapshot.data().content + '</div><div class="card-footer"><div class="row"><div class="col-6"></div><div class="col-6 text-right"><small>โดย: ' + querySnapshot.data().auther + '</small></div></div></div></div>')
         })
         $('#content-rows').fadeIn(200);
