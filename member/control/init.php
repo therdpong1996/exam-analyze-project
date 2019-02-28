a90f838242e71086ce51fcadd1e187fb19751f77
Therdpong Khangrang <therdpong1996@icloud.com>
2019-02-28 13:09:32 +0700

    Update init.php

--- member/control/init.php
+++ member/control/init.php
@@ -28,6 +28,6 @@
     //WEB URL
     $_G['furl'] = 'https://cat-rmutl.xyz/';
     $_G['url'] = 'https://cat-rmutl.xyz/member/';
-    $_G['webservice'] = 'https://b34031c8.ngrok.io/';
+    $_G['webservice'] = 'https://5c94020a.ngrok.io/';
 
     require_once 'functions.php';
