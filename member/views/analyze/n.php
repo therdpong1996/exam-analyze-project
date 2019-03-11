c41ad3edc079717cf1ef3b2259e495d4560fe170
Therdpong Khangrang <therdpong1996@icloud.com>
2019-03-11 15:36:08 +0700

    Update n.php

--- member/views/analyze/n.php
+++ member/views/analyze/n.php
@@ -295,17 +295,41 @@
             var actx = canvas2.getContext("2d");
             var actxChart = new Chart(actx, Aconfig);
 
-            canvas2.onclick = function(evt) {
-                var activePoints = actxChart.getElementsAtEvent(evt);
+            canvas.onclick = function(evt) {
+                var activePoints = ctxChart.getElementsAtEvent(evt);
                 var chartData = activePoints[0]['_chart'].config.data;
                 var idx = activePoints[0]['_index'];
-
                 var label = chartData.labels[idx];
                 var value = chartData.datasets[0].data[idx];
+                $('#exampleModal').modal('show')
+            };
 
-                var url = "http://example.com/?label=" + label + "&value=" + value;
-                console.log(url);
-                alert(url);
+            canvas2.onclick = function(evt) {
+                var aactivePoints = actxChart.getElementsAtEvent(evt);
+                var achartData = aactivePoints[0]['_chart'].config.data;
+                var aidx = aactivePoints[0]['_index'];
+                var alabel = achartData.labels[aidx];
+                var avalue = achartData.datasets[0].data[aidx];
+                $('#exampleModal').modal('show')
             };
         </script>
+        
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Datalist</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+                </div>
+            </div>
+        </div>
         
