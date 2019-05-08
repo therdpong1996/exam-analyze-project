d57b3f4805e806c82ceb6a306a5295fe2848bfb8
Therdpong Khangrang <therdpong1996@icloud.com>
2019-05-08 23:04:11 +0700

    update dimensional

--- member/views/subject.page.php
+++ member/views/subject.page.php
@@ -226,7 +226,7 @@
                                 <?php echo $rows['subject_invite_code']; ?>
                             </td>
                             <td class="text-right">
-                                <button id="signout-btn-<?php echo $rows['subject_id']; ?>" onclick="subject_signout(<?php echo $rows['subject_id']; ?>)" class="btn btn-warning btn-sm">Signout</button>
+                                <button id="signout-btn-<?php echo $rows['subject_id']; ?>" onclick="subject_signout(<?php echo $rows['subject_id']; ?>)" class="btn btn-warning btn-sm">Leave</button>
                                 <a href="?edit&subject_id=<?php echo $rows['subject_id']; ?>" class="btn btn-info btn-sm">Edit</a>
                                 <button id="delete-btn-<?php echo $rows['subject_id']; ?>" onclick="subject_delete(<?php echo $rows['subject_id']; ?>)" class="btn btn-danger btn-sm">Delete</button>
                             </td>
