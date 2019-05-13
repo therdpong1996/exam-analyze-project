c21590e442f46182c114678cd14a7aba4771ff84
Therdpong Khangrang <numlifez@icloud.com>
2019-05-13 19:36:37 +0700

    a

--- member/views/parts/footer.content.php
+++ member/views/parts/footer.content.php
@@ -43,7 +43,33 @@
     <div class="close" onclick="$('#train-alert').hide(200)"><i class="fa fa-times"></i></div>
   </div>
   <div class="content">
-    
+    <?php
+      $stmtoo = $_DB->prepare("SELECT * FROM examinations WHERE (examination_train = 0 OR examination_train IS NULL) AND examination_subject IN (SELECT subject_id FROM subject_owner WHERE uid =:uid)");
+      $stmtoo->bindParam(':uid', $_SESSION['uid']);
+      $stmtoo->execute();
+      while($rows = $stmtoo->fetch(PDO::FETCH_ASSOC)){
+        
+        $stmty = $_DB->prepare("SELECT COUNT(id) as c FROM answer_data WHERE examination = :examid");
+        $stmty->bindParam(':examid', $rows['examination_id']);
+        $stmty->execute();
+        $dataNum = $stmty->fetch(PDO::FETCH_ASSOC);
+
+        $stmtx = $_DB->prepare('SELECT COUNT(qa_id) AS qac FROM q_and_a WHERE qa_delete = 0 AND qa_exam = :exam');
+        $stmtx->bindParam(':exam', $rows['examination_id']);
+        $stmtx->execute();
+        $ttNum = $stmtx->fetch(PDO::FETCH_ASSOC);
+
+        $rate = ($ttNum['qac']*10);
+
+        if($dataNum['c'] >= $rate){
+?>
+        ข้อสอบ <?php echo $rows['examination_title']; ?> มีข้อมูลเพียงพอสำหรับการวิเคราะห์ข้อมูล
+        <a class="btn btn-success btn-sm" href="<?php url('examination/analyze/?examination_id='.$rows['examination_id'].'&overview'); ?>">วิเคราะห์ข้อมูล</a>
+        <hr>
+<?php
+        }
+      }
+    ?>
   </div>
 </div>
 <?php } ?>
